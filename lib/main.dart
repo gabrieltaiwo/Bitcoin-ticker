b98069594b5e47dc1fd079d31c80345857d2525e
gabrieltaiwo <gabrieltaiwo@outlook.com>
2020-04-21 09:45:58 +0100

    First commit

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
-          primaryColor: Colors.lightBlue,
-          scaffoldBackgroundColor: Colors.white),
+          primaryColor: Colors.deepPurple,
+          scaffoldBackgroundColor: Colors.purple[900]),
       home: PriceScreen(),
     );
   }
